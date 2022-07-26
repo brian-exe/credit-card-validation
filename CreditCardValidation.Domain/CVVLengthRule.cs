4e2b6dd3852cc3a8c6b375f6f30db9873c008a4b
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-07-25 23:11:15 -0300

    Initial Domain Model

---
+++ CreditCardValidation.Domain/CVVLengthRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardValidation.Domain
+{
+    public class CVVLengthRule
+    {
+        public int Length { get; set; }
+    }
+}
---
+++ CreditCardValidation.Domain/CardType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardValidation.Domain
+{
+    public class CardType
+    {
+        public string Name { get; set; }
+        public List<NumberPrefixRule> PrefixRules { get; set; }
+        public List<NumberLengthRule> LengthRules { get; set; }
+        public List<CVVLengthRule> CVVLengthRules { get; set; }
+        public bool LuhnValidation { get; set; }
+    }
+}
