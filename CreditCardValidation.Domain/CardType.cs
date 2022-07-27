5c07e20c7a1470559f3040ce1687e97a1403a782
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-07-27 02:34:11 -0300

    initialization for collections

--- CreditCardValidation.Domain/CardType.cs
+++ CreditCardValidation.Domain/CardType.cs
@@ -8,6 +8,13 @@ namespace CreditCardValidation.Domain
 {
     public class CardType
     {
+        public CardType()
+        {
+            PrefixRules = new List<NumberPrefixRule>();
+            LengthRules = new List<NumberLengthRule>();
+            CVVLengthRules = new List<CVVLengthRule>();
+            LuhnValidation = true;
+        }
         public string Name { get; set; }
         public List<NumberPrefixRule> PrefixRules { get; set; }
         public List<NumberLengthRule> LengthRules { get; set; }
--- CreditCardValidation.Models/GenericApplicationResponse.cs
+++ CreditCardValidation.Models/GenericApplicationResponse.cs
@@ -8,6 +8,10 @@ namespace CreditCardValidation.Models
 {
     public class GenericApplicationResponse<T> where T: class
     {
+        public GenericApplicationResponse()
+        {
+            Errors = new List<ApplicationError>();
+        }
         public bool Success { get; set; }
         public T Data { get; set; }
         public List<ApplicationError> Errors { get; set; }
