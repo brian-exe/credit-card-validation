3a4fed8017163629408ccc30a5e548e930f2a670
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-07-27 02:50:09 -0300

    Adding repositories and abstractions

---
+++ CreditCardValidation.Abstractions/Repositories/ICardTypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using CreditCardValidation.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardValidation.Abstractions.Repositories
+{
+    public interface ICardTypeRepository
+    {
+        IEnumerable<CardType> GetSupportedCardTypes();
+    }
+}
