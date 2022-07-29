4832c08451f47919dfd04f4bd7d7e53e6ae0aa9d
Ezequiel Aciar <ez_aciar@farmacity.com.ar>
2022-07-29 01:33:41 -0300

    Luhn algorithm + tests

---
+++ CreditCardValidation.Services/Common/LuhnValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardValidation.Services.Common
+{
+    public static class LuhnValidator
+    {
+        public static bool IsValidCardNumber(string cardNumber)
+        {
+			int nDigits = cardNumber.Length;
+
+			if (nDigits == 0 || nDigits == 1)
+				return false;
+
+			int nSum = 0;
+			bool shouldDoubleDigit = true;
+			for (int i = nDigits - 1; i > 0; i--)
+			{
+
+				int d = cardNumber[i-1] - '0'; //ascii substraction
+
+				if (shouldDoubleDigit)
+                {
+					d *= 2;
+					d -= (d > 9) ? 9 : 0;
+                }
+
+				nSum += d;
+
+				shouldDoubleDigit = !shouldDoubleDigit;
+			}
+			int checkDigit = cardNumber[nDigits - 1] - '0';
+			return ((nSum + checkDigit) % 10 == 0);
+		}
+    }
+}
---
+++ CreditCardValidation.Tests.Services/LuhnValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using CreditCardValidation.Services.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CreditCardValidation.Tests.Services
+{
+
+    public class LuhnValidatorTests
+    {
+
+        [Theory]
+        [InlineData("12345678910")]
+        [InlineData("123")]
+        [InlineData("0")]
+        [InlineData("999999999999999999")]
+        [InlineData("444444448888888889")]
+        public void ValidateCardNumber_WhenNumberIsInvalid_ReturnsFalse(string number)
+        {
+            Assert.False(LuhnValidator.IsValidCardNumber(number));
+        }
+        
+        [Theory]
+        [InlineData("4915205476757332")]
+        [InlineData("4915197626180459")]
+        [InlineData("4915202062685977")]
+        [InlineData("4915212892031284")]
+        [InlineData("4915195781996115")]
+        public void ValidateCardNumber_WhenNumberIsValid_ReturnsTrue(string number)
+        {
+            Assert.True(LuhnValidator.IsValidCardNumber(number));
+        }
+    }
+}
